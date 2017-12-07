ee76e2043daf6a2214d8b4abf6e7fefe6680efc5
D. Scott Williamson <spot1984@gmail.com>
2017-12-06 18:22:49 -0600

    ingest jekyll test files

---
+++ source/face/whoops/favico.bat
@@ -0,0 +1,3 @@
+::magick whoops256x256.png -alpha off -resize 256x256 -define icon:auto-resize="256,128,96,64,48,32,16" favicon.ico
+::magick whoops256x256.png -resize 256x256 -define icon:auto-resize="256,128,96,64,48,32,16" favicon.ico
+magick whoops256x256.png -alpha on -resize 256x256 -define icon:auto-resize="256,128,96,64,48,32,16" favicon.ico
