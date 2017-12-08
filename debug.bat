a713541c0087060985a466de4061f32448cffcd1
D. Scott Williamson <spot1984@gmail.com>
2017-12-08 16:51:44 -0600

    Added debug.bat file to launch development windows quickly

---
+++ debug.bat
@@ -0,0 +1,13 @@
+:: build website and debug web server
+start "Jekyll server" cmd.exe /K jekyll server
+
+:: in gh-pages directory, start command prompt, code, and explorer window
+start /D ..\gh-pages "gh-pages" code .
+explorer ..\gh-pages
+
+:: in master directory, start command prompt, code, and explorer window
+start "master" code -n .
+explorer .
+
+:: launch firefox tabs
+start "launch tabs" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -url https://spot1984.github.io/compulsively-creative/index.html -url http://localhost:4000/compulsively-creative/index.html
