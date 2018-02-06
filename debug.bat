b405825433936b94d708996b08c26e644f28de67
D. Scott Williamson <spot1984@gmail.com>
2018-02-05 20:08:23 -0600

    Changed host to 0.0.0.0 to debug on local network and added URL tab in browser for Bootstrap version of site

--- debug.bat
+++ debug.bat
@@ -1,5 +1,5 @@
 :: build website and debug web server
-start "Jekyll server" cmd.exe /K jekyll server
+start "Jekyll server" cmd.exe /K jekyll server --host 0.0.0.0
 
 :: in gh-pages directory, start command prompt, code, and explorer window
 start /D ..\gh-pages "gh-pages" code .
@@ -10,4 +10,4 @@ start "master" code -n .
 explorer .
 
 :: launch firefox tabs
-start "launch tabs" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -url https://spot1984.github.io/compulsively-creative/index.html -url http://localhost:4000/compulsively-creative/index.html
+start "launch tabs" "C:\Program Files (x86)\Mozilla Firefox\firefox.exe" -url https://spot1984.github.io/compulsively-creative/index.html -url http://localhost:4000/compulsively-creative/index.html -url http://localhost:4000/compulsively-creative/index-bs.html
